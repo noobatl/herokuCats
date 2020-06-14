32905f3a417063f7dd23c7b04e2f71ab6e410b7c
noobatl <info@noobusa.com>
2020-06-14 13:00:00 -0400

    added config

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-const connection = require("./config/connection.js");
+const connection = require("./config/connection");
+
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
